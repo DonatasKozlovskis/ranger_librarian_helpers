b6a7042a621a555f1ebd06c74cdc8346df2b7a65
Donatas Kozlovskis <donatas.kozlovskis@gmail.com>
2015-05-04 16:45:18 +0200

    working structure after refactoring, missing actionlib server

--- src/ranger_librarian.cpp
+++ src/ranger_librarian.cpp
@@ -3,66 +3,60 @@
 // CONSTRUCTOR
 RangerLibrarian::RangerLibrarian():
     nh_("~"), it_(nh_), nh_rate_(NODE_RATE),
-    depth_read_label_(false), read_success_(false),
-    robot_stuck_(false),
-    depth_below_counter_(0), depth_below_time_(0),
-    weight_change_(0),
-    last_label_time_(0), last_label_author_(""), last_label_number_(""), last_label_weight_(0),
+    read_label_(false), read_label_success_(false), weight_max_reached_(false),
 
     lr_(OCR_FRAME_SKIP, QUEUE_MAX_LENGTH, QUEUE_ACCEPT_RATE, IMG_WIDTH, IMG_HEIGHT)
 
 {
     string rgb_image_topic;
-    string depth_image_topic;
+    string depth_low_duration;
+    string depth_low_action;
     string scale_topic;
+    string scale_filtered_topic;
 
     // get parameters for subscribers, if not use defaults
-    nh_.param<string>("rgb_image", rgb_image_topic, "/usb_cam/image_raw");
-    nh_.param<string>("depth_image", depth_image_topic, "/camera/depth_registered/image_raw");
-    nh_.param<string>("scale", scale_topic, "/ranger_scale");
+    nh_.param<string>("rgb_image", rgb_image_topic, RGB_IMAGE_TOPIC);
+    nh_.param<string>("depth_low_duration", depth_low_duration, "/depth_below_timer/depth_low_duration");
+    nh_.param<string>("depth_low_action", depth_low_action,     "/depth_below_timer/depth_low_action");
+
+    nh_.param<string>("scale", scale_topic, SCALE_TOPIC);
+    nh_.param<string>("scale_filtered", scale_filtered_topic, SCALE_FILTERED_TOPIC);
 
 
     // Subscribers
     sub_rgb_    = it_.subscribe(rgb_image_topic, 1, &RangerLibrarian::rgb_callback, this);
-    sub_depth_  = it_.subscribe(depth_image_topic, 1, &RangerLibrarian::depth_callback, this);
-    sub_scale_  = nh_.subscribe<const std_msgs::Float64&>(scale_topic, 1, &RangerLibrarian::scale_callback, this);
+
+    sub_depth_low_duration_  = nh_.subscribe<const std_msgs::Float64&>(depth_low_duration, 1, &RangerLibrarian::depth_low_duration_callback, this);
+    sub_depth_low_action_  = nh_.subscribe<const std_msgs::String&>(depth_low_action, 2, &RangerLibrarian::depth_low_action_callback, this);
+
+    sub_scale_  =           nh_.subscribe<const std_msgs::Float64&>(scale_topic, 1, &RangerLibrarian::scale_callback, this);
+    sub_scale_filtered_  =  nh_.subscribe<const ranger_librarian::WeightFiltered&>(scale_filtered_topic, 1, &RangerLibrarian::scale_filtered_callback, this);
 
     // get the rest of paramters
-    nh_.param("depth_below_read", depth_low_time_read, int(2));
-    nh_.param("depth_below_stuck", depth_low_time_stuck, int(5));
-    nh_.param("depth_below_threshold", depth_below_threshold, double(0.2));
+    nh_.param("weight_max_allowed", weight_max_allowed_,     int(5));
+    nh_.param("time_depth_low_read", time_depth_low_read_,  int(2));
+    nh_.param("time_wait_read_label", time_wait_read_label_, int(5));
+    nh_.param("time_wait_add_book", time_wait_add_book_,    int(8));
 
-    nh_.param("read_time_label", read_time_label, int(5));
-
-
-
-    // get the scale paramters
-    nh_.param("weight_sensitivity", weight_sensitivity, double(0.15));
-    nh_.param("weight_max_allowed", weight_max_allowed, int(6));
-    nh_.param("weight_wait_time_book", weight_wait_time_book, int(8));
-
-
-
-    // calculate max used time to init circular buffer capacities
-    int max_time = max(max(depth_low_time_read,depth_low_time_stuck), weight_wait_time_book);
-
-    // set circular buffer sizes
-    weight_measures.set_capacity((int)(NODE_RATE*max_time));
-    depth_measures.set_capacity((int)(NODE_RATE*max_time));
 }
 
-// DESTRUCTOR
+/// DESTRUCTOR
 RangerLibrarian::~RangerLibrarian()
 {
 
 }
 
-
-// CALLBACKS
+/////////////////////////////////////////////////////////////////////
+/// CALLBACKS
 void RangerLibrarian::rgb_callback(const sensor_msgs::ImageConstPtr& msg) {
 
-    // get mutable cv image
-    //cv_bridge::CvImagePtr cv_ptr;
+    if (DEBUG) {
+        printf("rgb_callback msg received.\n");
+    }
+
+
+
+    // get mutable cv image from cv_bridge::CvImagePtr cv_ptr;
     try {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     } catch (cv_bridge::Exception& e) {
@@ -70,17 +64,16 @@ void RangerLibrarian::rgb_callback(const sensor_msgs::ImageConstPtr& msg) {
         return;
     }
 
-
     // prepare user output image view
     cv::Mat userImage = cv_ptr->image.clone();
     lr_.prepareUserImage(userImage);
 
 
-    if (depth_read_label_) {
+    if (read_label_) {
         putText(cv_ptr->image, "Reading", cv::Point(15,15), CV_FONT_HERSHEY_COMPLEX, 2, CV_RGB(0,0,250));
     }
-    if (robot_stuck_) {
-        putText(cv_ptr->image, "Stuck", cv::Point(15,15), CV_FONT_HERSHEY_COMPLEX, 2, CV_RGB(0,0,250));
+    if (read_label_success_) {
+        putText(cv_ptr->image, "read_label_success_", cv::Point(15,15), CV_FONT_HERSHEY_COMPLEX, 2, CV_RGB(0,0,250));
     }
 
     // Update GUI Window
@@ -89,78 +82,98 @@ void RangerLibrarian::rgb_callback(const sensor_msgs::ImageConstPtr& msg) {
     cv::waitKey(3);
 }
 
-void RangerLibrarian::depth_callback(const sensor_msgs::ImageConstPtr& msg) {
+void RangerLibrarian::depth_low_duration_callback(const std_msgs::Float64& msg) {
 
-    // Data access using CV bridge, shared, returning a const CvImage
-    cv_bridge::CvImageConstPtr depth_img_ptr;
-    try
-    {
-        depth_img_ptr = cv_bridge::toCvShare(msg, msg->encoding);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not deserialize depth image: %s", e.what());
-        return;
-    }
+//    if (DEBUG) {
+//        printf("depth_below_duration msg received:  %0.2f\n", msg.data);
+//    }
 
-    // copy image matrix header
-    Mat depth_image = depth_img_ptr->image;
-    // create center image region taking 1/(3^2) of image
-    // e.g. from cols/3 to cols/3*2 and from rows/3 to rows/3*2
-    Rect center_region = Rect(depth_image.cols/3, depth_image.rows/3, depth_image.cols/3, depth_image.rows/3);
+    double depth_below_duration = msg.data;
 
-    // create a matrix header of a part of the bigger matrix
-    Mat depth_image_center( depth_image, center_region );
-    // replace NaN with zeros
-    depth_image_center.setTo(0, depth_image_center!=depth_image_center);
+//    if (depth_below_duration > time_depth_low_read_) {
+//        read_label_ = true;
+//    } else {
+//        read_label_ = false;
+//    }
+}
 
-    // calculate depth image center mean
-    double depth_center_mean = mean(depth_image_center)[0];
+void RangerLibrarian::depth_low_action_callback(const std_msgs::String& msg) {
 
-    // convert if needed
-    if (msg->encoding == "16UC1") {
-        // depths are 16-bit unsigned ints, in mm. Convert to 32-bit float (meters)
-        depth_center_mean = depth_center_mean / 1000; // (mm to m)
+    string depth_low_action = msg.data;
+
+    if (DEBUG) {
+        printf("depth_low_action msg received:  %s\n", depth_low_action.c_str());
     }
 
-    // add to circular buffer
-    depth_measures.push_front(depth_center_mean);
-
-    // Logical control
-
-    // last known time when depth is bigger than threshold
-    if (depth_center_mean > depth_below_threshold) {
-        depth_below_time_ = ros::Time::now().toSec();
+    if (depth_low_action.compare("stop")==0 ) {
+        read_label_ = true;
+    } else {
+        read_label_ = false;
     }
 
+    if (read_label_) {
+        std::cout <<  "try read label " << std::endl;
+        read_label_success_ = book_read_label();
+        if (read_label_success_) {
+             std::cout <<  "read label success! waiting for book " << std::endl;
 
+             if (book_read_weight()) {
+                 std::cout <<  "read book added! " << std::endl;
+             } else {
+                 std::cout <<  "read book add failed! " << std::endl;
+             }
+        } else {
+            std::cout <<  "read label failed! " << std::endl;
+        }
+
+        read_label_ = false;
+
+    }
 
 }
 
 void RangerLibrarian::scale_callback(const std_msgs::Float64& msg) {
 
-
     double weight_current = msg.data;
 
-    // add to circular buffer
-    weight_measures.push_front(weight_current);
+    if (DEBUG) {
+        printf("Scale msg received:  %0.2f\n", weight_current);
+    }
 
-    weight_last_ = weight_current;
+    if (weight_current > weight_max_allowed_) {
+        weight_max_reached_ = true;
+    } else {
+        weight_max_reached_ = false;
+    }
 
-//    weight_change_ =  weight_current - weight_last_;
-
-//    if (abs(weight_change_) > weight_sensitivity) {
-//        // significant change;
-//        weight_change_ = 0;
-//    } else {
-//        // not significant weight increase
-//        weight_change_ = 0;
-//    }
-
-
+    // implement control of max weight reached
 }
 
+void RangerLibrarian::scale_filtered_callback(const ranger_librarian::WeightFiltered& msg) {
 
+    double weight_stable = msg.weight_stable;
+    double weight_change = msg.weight_change;
+    bool weight_changed =  msg.weight_changed;
+
+    if (DEBUG) {
+        printf("scale_filtered_callback msg received:  %0.3f\n", weight_stable);
+    }
+
+    if (weight_changed) {
+        if (weight_change > 0) {
+            last_book_add_time_ = msg.change_time;
+            std::cout << "book added" << std::endl;
+        } else {
+            std::cout << "book removed" << std::endl;
+        }
+    } else {
+        // no change
+    }
+
+}
+/////////////////////////////////////////////////////////////////////
+
+/// SPINNER
 int RangerLibrarian::run() {
 
     long int current_time;
@@ -169,32 +182,7 @@ int RangerLibrarian::run() {
 
         current_time = ros::Time::now().toSec();
 
-        std::cout << "move around" << std::endl;
-
-
-        if ( depth_below_time_!=0 && ( current_time-depth_below_time_>= depth_low_time_read)) {
-            depth_read_label_ = true;
-            depth_below_time_ = 0;
-        }
-        if (depth_read_label_) {
-            std::cout <<  "try read label " << std::endl;
-            if (read_label()) {
-                 std::cout <<  "read label success! waiting for book " << std::endl;
-
-                 if (read_book_weight()) {
-                     std::cout <<  " book added! " << std::endl;
-                 } else {
-                     std::cout <<  " book added failed! " << std::endl;
-                 }
-
-            } else {
-                std::cout <<  "read label failed! " << std::endl;
-            }
-
-
-            depth_read_label_ = false;
-
-        }
+//        std::cout << "move around" << std::endl;
 
 
         ros::spinOnce();
@@ -203,130 +191,58 @@ int RangerLibrarian::run() {
 
 }
 
-bool RangerLibrarian::read_label() {
+bool RangerLibrarian::book_read_label() {
 
-    read_success_ = false;
+    bool read_success = false;
 
-    long int current_time = ros::Time::now().toSec();
-    long int start_time = current_time;
+    ros::Time start_time = ros::Time::now();
+    ros::Time run_time = ros::Time::now();
 
 
     lr_.readLabel(true);
 
-    while (!read_success_&& (current_time-start_time < read_time_label) && ros::ok() ) {
+    while (!read_success && (run_time-start_time < ros::Duration(time_wait_read_label_)) && ros::ok() ) {
 
-        read_success_ = lr_.processFrame(cv_ptr->image);
+        read_success = lr_.processFrame(cv_ptr->image);
 
-        ros::spinOnce();
-        nh_rate_.sleep();
-        current_time = ros::Time::now().toSec();
+        ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
+        run_time = ros::Time::now();
     }
 
-    if (read_success_){
-        //update last read values
-        last_label_time_ = ros::Time::now().toSec();
-        last_label_author_ = lr_.getAuthor();
-        last_label_number_ = lr_.getCallNumber();
-        last_label_weight_ = weight_last_;
+    if (read_success){
+        //update last book values
+        last_book_add_time_ = ros::Time::now();
+        last_book_add_.author = lr_.getAuthor();
+        last_book_add_.callNumber = lr_.getCallNumber();
+        last_book_add_.weight = 0;
     } else {
-        last_label_time_ = 0;
-        last_label_weight_ = 0;
-        last_label_author_ = "";
-        last_label_number_ = "";
+        last_book_add_time_ = ros::Time(0);
+        last_book_add_.weight = 0;
+        last_book_add_.author = "";
+        last_book_add_.callNumber = "";
     }
 
     lr_.reset();
 
-    return read_success_;
+    return read_success;
 }
-bool RangerLibrarian::read_book_weight() {
+
+bool RangerLibrarian::book_read_weight() {
 
     bool book_added = false;
 
-    long int current_time = ros::Time::now().toSec();
-    long int start_time = current_time;
+    ros::Time start_time = ros::Time::now();
+    ros::Time run_time = ros::Time::now();
 
-    double weight_before_book = weight_last_;
+    while (!book_added && (run_time-start_time < ros::Duration(time_wait_read_label_)) && ros::ok() ) {
 
-    while (!book_added && ( (current_time-start_time)< weight_wait_time_book) && ros::ok() ) {
-
-        double mean_weight = weight_homogeneous(0,60);
-
-
-        if (mean_weight && ( (mean_weight - weight_before_book) > weight_sensitivity )) {
-
-
-            std::cout << "mean weight: " << mean_weight << " before: " << weight_before_book << std::endl;
-
-//            bool weight_equal = true;
-//            int counter = 0;
-//            double weight_sum = 0;
-//            int window_size = 60;
-//            int frame_begin = 0;
-
-//            auto it=weight_measures.begin() + frame_begin;
-
-//            double weight_previous = *it;
-//            while( weight_equal && counter < window_size) {
-
-//                weight_sum += (*it);
-
-//                double  weight_diff = (*it) - weight_previous;
-//                bool two_frames_equal = std::abs(weight_diff) < weight_sensitivity;
-//                weight_equal = weight_equal && two_frames_equal;
-
-//                std::cout << (*it) << " " << *(it+1) << " " << weight_diff << " " << two_frames_equal << std::endl;
-
-//                weight_previous = (*it);
-//                it++;
-//                counter++;
-//            }
-
-
+        if (last_book_add_time_ > start_time ) {
             book_added = true;
         }
 
-
-        ros::spinOnce();
-        nh_rate_.sleep();
-        current_time = ros::Time::now().toSec();
+        ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
+        run_time = ros::Time::now();
     }
 
     return book_added;
-}
-
-double RangerLibrarian::weight_homogeneous(int frame_begin, int window_size) {
-
-    double weight_mean = std::nan("");
-
-
-    if (weight_measures.size() > (frame_begin+window_size)) {
-
-        bool weight_equal = true;
-        int counter = 0;
-        double weight_sum = 0;
-
-        auto it=weight_measures.begin() + frame_begin;
-
-        double weight_previous = *it;
-        while( weight_equal && counter < window_size) {
-
-            weight_sum += (*it);
-
-            double  weight_diff = (*it) - weight_previous;
-            bool two_frames_equal = std::abs(weight_diff) < weight_sensitivity;
-            weight_equal = weight_equal && two_frames_equal;
-
-
-            weight_previous = (*it);
-            it++;
-            counter++;
-        }
-
-        if (weight_equal) {
-            weight_mean = weight_sum / window_size;
-        }
-    }
-
-    return weight_mean;
 }
--- src/ranger_librarian.h
+++ src/ranger_librarian.h
@@ -3,10 +3,14 @@
 
 // ROS
 #include <ros/ros.h>
+#include <ros/callback_queue.h>
 
 // ROS messages
 #include <sensor_msgs/image_encodings.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
+#include <ranger_librarian/WeightFiltered.h>
+
 
 //CV bridge
 #include <image_transport/image_transport.h>
@@ -21,26 +25,42 @@
 #include "label_reader.h"
 #include <cmath>
 
-// camera parameters
+using std::string;
+using namespace cv;
+
+/**@brief Flag for printing debug messages.*/
+bool const DEBUG = false;
+
+/**@brief Camera parameters.*/
 static const int IMG_WIDTH = 640;           //all formats: "luvcview -d /dev/video0 -L"
 static const int IMG_HEIGHT = 480;
 
-// OCR PARAMS
+/**@brief OCR parameters.*/
 static const int OCR_FRAME_SKIP = 5;        // parameter to process each xx frame with OCR
 static const int QUEUE_MAX_LENGTH = 10;     // how many historical values to keep in queue
 static const double QUEUE_ACCEPT_RATE = 0.7;// last repeated element acceptance rate
 
+/**@brief Default rosparam server values.*/
+static const string RGB_IMAGE_TOPIC = "/usb_cam/image_raw";
+static const string SCALE_TOPIC = "/scale";
+static const string SCALE_FILTERED_TOPIC = "/scale_filtered";
+
 // node rate
 static const int NODE_RATE = 31;
-
-using std::string;
-using namespace cv;
 
 // Struct for books
 struct Book {
     string author;
     string callNumber;
     double weight;
+};
+
+// navigator ACTIONS
+enum NavigatorAction
+{
+    NAVIGATOR_STOP,
+    NAVIGATOR_MOVE,
+    NAVIGATOR_FINISH
 };
 
 /// CLASS
@@ -55,9 +75,13 @@ public:
 
     // Callbacks
     void rgb_callback(const sensor_msgs::ImageConstPtr& msg);
-    void depth_callback(const sensor_msgs::ImageConstPtr& msg);
-    void scale_callback(const std_msgs::Float64& msg);
 
+    void depth_low_duration_callback(const std_msgs::Float64& msg);
+    void depth_low_action_callback(const std_msgs::String& msg);
+    void scale_callback(const std_msgs::Float64& msg);
+    void scale_filtered_callback(const ranger_librarian::WeightFiltered& msg);
+
+    // methods
     int run();
 
 private:
@@ -74,70 +98,50 @@ private:
 
     // Subscribers
     image_transport::Subscriber sub_rgb_;
-    image_transport::Subscriber sub_depth_;
+
+    ros::Subscriber sub_depth_low_duration_;
+    ros::Subscriber sub_depth_low_action_;
     ros::Subscriber sub_scale_;
+    ros::Subscriber sub_scale_filtered_;
 
 
     // NODE PARAMETERS
-
     // depth image control parameters;
-    int depth_low_time_read;           // time (s) for depth val = 0 to start label read
-    int depth_low_time_stuck;          // time (s) for depth val = 0 to reset navigato
-    double depth_below_threshold;   // depth threshold to to assume depth val = 0
 
-    // book add parameters
-    double weight_sensitivity;      // assume weight change when abs(diff) > sensitivity
-    int weight_wait_time_book;      // time (s) to wait for adding a book
-    int weight_max_allowed;         // maximum weight allowed
-
+    //  scale callback
+    int weight_max_allowed_;         // maximum weight allowed
 
     // read label parameters
-    int read_time_label;            // time (s) to wait for reading a book label
-
+    int time_depth_low_read_;            // time (s) for depth val = 0 to start label read
+    int time_wait_read_label_;           // time (s) to wait for reading a book label
+    int time_wait_add_book_;             // time (s) to wait for adding a book
 
 
     //CLASS Member variables
 
     // Label reader object
     LabelReader lr_;
+    // pointer to obtained cv image
     cv_bridge::CvImagePtr cv_ptr;
 
     // control boolean variables
-    bool depth_read_label_;
-    bool read_success_;
-    bool robot_stuck_;
+    bool read_label_;
+    bool read_label_success_;
+    bool weight_max_reached_;
 
+    // book struct object
+    Book last_book_add_;
+    ros::Time last_book_add_time_;
 
-    int depth_below_counter_;
-    long int depth_below_time_;
+    NavigatorAction action_last_;
+    NavigatorAction action_current_;
 
-
-
-
-
-
-    double weight_change_;
-    double weight_last_;
-
-
-    vector<Book> book_list_;
-
-    // book object
-    string last_label_author_;
-    string last_label_number_;
-    double last_label_weight_;
-    double last_label_time_;
-
-
-    // circular buffers (queue) of fixed lenght
-    boost::circular_buffer<double> weight_measures;
-    boost::circular_buffer<double> depth_measures;
-
+    // list of books
+    std::vector<Book> book_list_;
 
     // methods
-    bool read_label();
-    bool read_book_weight();
-    double weight_homogeneous(int frame_begin, int window_size);
+    bool book_read_label();
+    bool book_read_weight();
 
 };
 
